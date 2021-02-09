6d860b2818b23d23514246e5099b0a294687e1dd
deepak bhatt <deepakbhattmits@gmail.com>
2021-02-09 09:32:52 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @format */
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+module.exports = {
+	webpack: (config) => {
+		config.plugins.push(
+			new SWPrecacheWebpackPlugin({
+				minify: true,
+				staticFileGlobsIgnorePatterns: [/\.next\//],
+				runtimeCaching: [
+					{
+						handler: 'networkFirst',
+						urlPattern: /^https?.*/,
+					},
+				],
+			})
+		);
+		return config;
+	},
+};
