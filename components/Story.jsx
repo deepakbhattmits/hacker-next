278167316a9969e2365af9ec7d375668194bdd42
deepak bhatt <deepakbhattmits@gmail.com>
2021-02-09 09:33:13 +0530

    Update Story.jsx

--- components/Story.jsx
+++ components/Story.jsx
@@ -1,35 +1,46 @@
 /** @format */
 import Link from 'next/link';
-const Story = ({ story: { id, title, url, points, comments_count } }) => {
-	console.log('STORY : ', id, title, url, points, comments_count);
-	return (
-		<div className='story'>
-			<h2 className='story-title'>
-				<a href={url}>{title}</a>
-			</h2>
-			<div className='story-details'>
-				<span>{points || 0} points</span>
-				<Link href={`/story?id=${id}`}>{comments_count || 0} comments</Link>
-			</div>
-			<style jsx>{`
-				.story {
-					padding: 1em 0;
-				}
-				.story-title {
-					font-size: 1rem;
-					font-weight: 400;
-					margin: 0 0 0.5em 0;
-				}
-				.story-title a {
-					color: #333;
-					text-decoration: none;
-				}
-				.story-title a:over {
-					color: #444;
-					text-decoration: initial;
-				}
-			`}</style>
+const Story = ({ story: { id, title, url, points, comments_count } }) => (
+	<div className='story'>
+		<h2 className='story-title'>
+			<a href={url}>{title}</a>
+		</h2>
+		<div className='story-details'>
+			<span>{points || 0} points</span>
+			<Link href={`/story?id=${id}`}>
+				<a>{comments_count || 0} comments</a>
+			</Link>
 		</div>
-	);
-};
+		<style jsx>{`
+			.story {
+				padding: 1em;
+			}
+			.story-title {
+				font-size: 1rem;
+				font-weight: 400;
+				margin: 0 0 0.5em 0;
+			}
+			.story-title a {
+				color: #333;
+				text-decoration: none;
+			}
+			.story-title a:hover,
+			.story-details a:hover {
+				color: #444;
+				text-decoration: initial;
+			}
+			.story-details {
+				font-size: 0.8rem;
+				font-weight: bold;
+			}
+			.story-details span {
+				margin-right: 1em;
+			}
+			.story-details a {
+				color: #6600ff;
+				text-decoration: none;
+			}
+		`}</style>
+	</div>
+);
 export default Story;
