bc65a9289b2f138b69a257886f7cec914eeaea30
deepak bhatt <deepakbhattmits@gmail.com>
2021-02-09 09:33:22 +0530

    Create CommentList.jsx

---
+++ components/CommentList.jsx
@@ -0,0 +1,11 @@
+/** @format */
+
+import Comment from './Comment';
+
+export default ({ comments }) => (
+	<>
+		{comments.map((comment) => (
+			<Comment key={comment.id} comment={comment} />
+		))}
+	</>
+);
